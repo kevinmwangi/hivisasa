69e2b4b4d9bc18914c57c5f670b2703eca1aaece
kevinmwangi <kevinnmwangi@gamil.com>
2017-01-17 14:20:29 +0300

    page fixes

--- modules/routes.js
+++ modules/routes.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import { Route, IndexRoute } from 'react-router'
-import App from './App'
-import About from './About'
-import Repos from './Repos'
-import Repo from './Repo'
-import Home from './Home'
-import ArticleNews from './ArticleNews'
+import React from "react";
+import {Route, IndexRoute} from "react-router";
+import App from "./App";
+import About from "./About";
+import Repos from "./Repos";
+import Repo from "./Repo";
+import Home from "./Home";
+import ArticleNews from "./ArticleNews";
 
 module.exports = (
     <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/repos" component={Repos}>
-        <Route path="/repos/:userName/:repoName" component={Repo}/>
-      </Route>
-      <Route path="/about" component={About}/>
-      <Route path="/articleNews" component={ArticleNews}>
-          <Route path="/articleNews/:nid" component={ArticleNews}/>
-      </Route>
+        <IndexRoute component={Home}/>
+        <Route path="/repos" component={Repos}>
+            <Route path="/repos/:userName/:repoName" component={Repo}/>
+        </Route>
+        <Route path="/about" component={About}/>
+        <Route path="/articleNews" component={ArticleNews}>
+            <Route path="/articleNews/:nid" component={ArticleNews}/>
+        </Route>
     </Route>
-)
+);
